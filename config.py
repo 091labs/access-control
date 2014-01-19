2869f2c9b6b595e04dce3dfc33aa3c80cf7651cd
Tom Leaman <tom@tomleaman.co.uk>
2014-01-19 17:57:20 +0000

    Prepare for deployment

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'users.db')
+SQLALCHEMY_DATABASE_URI = 'mysql://root:changeme@localhost/users'
 
 CSRF_ENABLED = True
-SECRET_KEY = '69fe28e5-4dba-4752-abf4-8be4fd2f18a1'
+SECRET_KEY = 'changeme'
--- run_flask.py
+++ run_flask.py
@@ -2,4 +2,4 @@
 
 from access import app
 
-app.run(debug=True)
+app.run(host='0.0.0.0')
